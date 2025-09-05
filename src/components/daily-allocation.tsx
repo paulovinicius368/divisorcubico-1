a6691f11b138843e42275ee0383a3fd66ab94e5b
paulo vinicius <paulovinicius368@gmail.com>
2025-09-05 18:48:38 +0000

    Ao salvar o apontamento limpar o resultado

--- src/components/daily-allocation.tsx
+++ src/components/daily-allocation.tsx
@@ -116,6 +116,7 @@ export default function DailyAllocation({ onSave }: DailyAllocationProps) {
         form.getValues("totalVolume"),
         allocationResult
       );
+      setAllocationResult(null);
     }
   }
 
